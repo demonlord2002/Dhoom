93409bf1dd9a6c5b2efb96ca0e60d146cfa982a5
demonlord2002 <drdoom2003p@gmail.com>
2025-11-06 17:49:57 +0530

    Create config.py

---
+++ bot/config.py
@@ -0,0 +1,12 @@
+import os
+
+API_ID = int(os.environ.get("API_ID", "YOUR_API_ID"))
+API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
+BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "YOUR_CHANNEL_ID"))
+
+MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
+DB_NAME = "telegram_files"
+COLLECTION_NAME = "files"
+
+DOMAIN = os.environ.get("DOMAIN", "vignesh-v2.netlify.app")
