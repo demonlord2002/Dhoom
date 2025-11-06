faa62b1d32571fb6a82e3091deaa9b43e9e681d6
demonlord2002 <drdoom2003p@gmail.com>
2025-11-06 19:19:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 API_ID = int(os.environ.get("API_ID", "YOUR_API_ID"))
 API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN")
-BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "-1003213565850"))  # new working channel
+BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "-1003293810900"))  # new working channel
 
 MONGO_URI = os.environ.get("MONGO_URI", "mongodb://localhost:27017/")
 DB_NAME = "telegram_files"
